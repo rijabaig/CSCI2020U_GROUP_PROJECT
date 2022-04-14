0d82c43c82fe97e36b5c1fc56151618fe35fe2d8
rijabaig <90227392+rijabaig@users.noreply.github.com>
2022-04-14 18:29:23 -0400

    Add files via upload

---
+++ Messenger/Messenger/src/Client/Main.java
@@ -0,0 +1,25 @@
+package Client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
+        primaryStage.setTitle("OntarioTech Chat Application");
+        primaryStage.setScene(new Scene(root, 330, 560));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
